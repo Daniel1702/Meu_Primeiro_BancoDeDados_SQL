d98f3441f397b8610188abc294cf8996c691e4a9
Daniel1702 <106408440+Daniel1702@users.noreply.github.com>
2023-03-20 21:00:50 -0300

    Add files via upload

---
+++ BancoDados_Cadastro-MYSQL.sql
@@ -0,0 +1,22 @@
+create database cadastros
+default character set utf8
+default collate utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS pessoas (
+    id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(30) NOT NULL,
+    nascimento DATE,
+    sexo ENUM('M', 'F'),
+    peso DECIMAL(5 , 2 ),
+    altura DECIMAL(3 , 2 ),
+    nacionalidade VARCHAR(20) DEFAULT 'Brasil',
+    PRIMARY KEY (id)
+)  DEFAULT CHARSET=UTF8; 
+
+INSERT INTO pessoas
+(nome, nascimento, sexo, peso, altura, nacionalidade)
+VALUES
+('Godofredo', '1984-01-02', 'M', '78.5', '1.83', 'Brasil'),
+('Maria', '1994-05-26', 'F', '85.3', '1.69', default);
+
+SELECT * FROM pessoas;
